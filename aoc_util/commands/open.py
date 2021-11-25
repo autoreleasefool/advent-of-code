9d73d162cde36a353880c69750eb83217aa8fb2b
Joseph Roque <contact@josephroque.ca>
2021-11-25 09:35:32 -0800

    Open command

---
+++ aoc_util/commands/open.py
@@ -0,0 +1,10 @@
+import webbrowser
+
+from aoc_util.commands.base_command import BaseCommand
+from aoc_util.session import Session
+
+
+class Open(BaseCommand):
+
+  def run(self, session: Session):
+    webbrowser.open(session.challenge.url, new=2)
