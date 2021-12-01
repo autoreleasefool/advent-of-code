2217cd717c94e43dfa8071ffe8be508df2793b3c
Joseph Roque <contact@josephroque.ca>
2021-11-30 21:24:56 -0800

    Python, 2021, day 1

---
+++ 2021/day_01/python/day01.py
@@ -0,0 +1,21 @@
+from aoc import AOC
+import math
+
+
+aoc = AOC(year=2021, day=1)
+series = aoc.load().numbers()
+
+
+# Part 1
+
+increments = sum(1 for x in range(len(series) - 1) if series[x] < series[x + 1])
+aoc.p1(increments)
+
+# Part 2
+
+increments = sum(
+    1
+    for x in range(3, len(series))
+    if sum(series[x - 3 : x]) < sum(series[x - 2 : x + 1])
+)
+aoc.p2(increments)
