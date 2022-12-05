2f1d16ed41a2a633c07e4f03774b13f30275985e
Joseph Roque <contact@josephroque.ca>
2022-12-04 20:47:19 -0800

    Convert indentation to spaces

--- 2022/day_04/python/day04.py
+++ 2022/day_04/python/day04.py
@@ -4,26 +4,26 @@ aoc = AOC(year=2022, day=4)
 data = aoc.load()
 
 def get_range(elf):
-	ends = elf.split('-')
-	return range(int(ends[0]), int(ends[1]) + 1)
+    ends = elf.split('-')
+    return range(int(ends[0]), int(ends[1]) + 1)
 
 def intersect(r1, r2):
-	return range(max(r1.start, r2. start), min(r1.stop, r2.stop)) or None
+    return range(max(r1.start, r2. start), min(r1.stop, r2.stop)) or None
 
 overlaps = 0
 for elves in data.lines():
-	elf1, elf2 = elves.split(',')
-	elf1, elf2 = get_range(elf1), get_range(elf2)
+    elf1, elf2 = elves.split(',')
+    elf1, elf2 = get_range(elf1), get_range(elf2)
 
-	if intersect(elf1, elf2) == elf1 or intersect(elf1, elf2) == elf2:
-		overlaps += 1
+    if intersect(elf1, elf2) == elf1 or intersect(elf1, elf2) == elf2:
+        overlaps += 1
 aoc.p1(overlaps)
 
 overlaps = 0
 for elves in data.lines():
-	elf1, elf2 = elves.split(',')
-	elf1, elf2 = get_range(elf1), get_range(elf2)
+    elf1, elf2 = elves.split(',')
+    elf1, elf2 = get_range(elf1), get_range(elf2)
 
-	if intersect(elf1, elf2):
-		overlaps += 1
+    if intersect(elf1, elf2):
+        overlaps += 1
 aoc.p2(overlaps)
