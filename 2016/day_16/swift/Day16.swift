2535b03d9b6e67c8d77e4b2f686f1fa0238555de
Joseph Roque <github@josephroque.ca>
2024-12-02 22:05:03 -0800

    feat(swift): 2016, day 16

---
+++ 2016/day_16/swift/Day16.swift
@@ -0,0 +1,56 @@
+import AdventSupport
+import Foundation
+
+public class Year2016Day16: Solver {
+	public required init() {}
+
+	public func setUp(_ input: inout Input) async throws {
+		input = Input(contents: "01111001100111011")
+	}
+
+	// MARK: Part 1
+
+	public func solvePart1(_ input: Input) async throws -> String? {
+		fillDisk(initialState: input.contents, diskSize: 272)
+	}
+
+	// MARK: Part 2
+
+	public func solvePart2(_ input: Input) async throws -> String? {
+		fillDisk(initialState: input.contents, diskSize: 35651584)
+	}
+
+	// MARK: Helpers
+
+	private func fillDisk(initialState: String, diskSize: Int) -> String {
+		var state = initialState
+		while state.count < diskSize {
+			state = expandState(state)
+		}
+
+		return generateChecksum(state: state, diskSize: diskSize)
+	}
+
+	private func expandState(_ state: String) -> String {
+		let a = state
+		let b = String(a.reversed().map { $0 == "0" ? "1" : "0" })
+		return "\(a)0\(b)"
+	}
+
+	private func generateChecksum(state: String, diskSize: Int) -> String {
+		func reduce(_ state: String) -> String {
+			String(
+				state
+					.chunks(ofCount: 2)
+					.map { $0.first == $0.last ? "1" : "0" }
+			)
+		}
+
+		var checksum = reduce(String(state.prefix(diskSize)))
+		while checksum.count.isMultiple(of: 2) {
+			checksum = reduce(checksum)
+		}
+
+		return checksum
+	}
+}
